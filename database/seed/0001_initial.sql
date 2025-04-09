3e4f44d4b461bbdb06af266666882871caa86260
Vladislav Hadzhyiski <vladislav.hadzhiyski@gmail.com>
2025-04-09 13:59:21 +0300

    update seed with vaccinations & treatments

--- database/seed/0001_initial.sql
+++ database/seed/0001_initial.sql
@@ -15,24 +15,8 @@ values (
         'Golden Retriever',
         '{cream}',
         null
-    );
-insert into public.pet_markings(id, code, place, application_date)
-values (
-        1,
-        '102938475610293 BG',
-        'left neck',
-        '2022-05-06'
-    );
-insert into public.pets (
-        "name",
-        "dob",
-        "sex",
-        "species",
-        "breed",
-        "colors",
-        "notes"
-    )
-values (
+    ),
+    (
         'Blaze',
         '2024-03-12',
         'female',
@@ -49,6 +33,13 @@ insert into public.pet_markings(
         "application_date"
     )
 values (
+        1,
+        '102938475610293 BG',
+        'left neck',
+        'microchip',
+        '2022-05-06'
+    ),
+    (
         2,
         '019283746501923 BG',
         'left neck',
@@ -94,18 +85,8 @@ values (
         null,
         null,
         null
-    );
-insert into public.veterinarians (
-        "name",
-        "address",
-        "postcode",
-        "city",
-        "country",
-        "phone",
-        "email",
-        "website"
-    )
-values (
+    ),
+    (
         'Dr Georgiev',
         'Kutlovica 12 str.',
         '3400',
@@ -123,13 +104,135 @@ insert into public.passports (
         "owner1_id",
         "owner2_id"
     )
-values ('BG01VP112233', '2022-05-06', 1, 1, 1, null);
-insert into public.passports (
-        "serial_number",
-        "issue_date",
-        "issued_by",
-        "pet_id",
-        "owner1_id",
-        "owner2_id"
+values ('BG01VP112233', '2022-05-06', 1, 1, 1, null),
+    ('BG01VK990087', '2024-04-22', 2, 2, 1, null);
+insert into public.anti_parasite_treatments (
+        name,
+        manufacturer,
+        administered_on,
+        administered_by,
+        pet_id
     )
-values ('BG01VK990087', '2024-04-22', 2, 2, 1, null);
+values ('Advantix', 'Bayer', '2022-07-04 00:00:00', 1, 1),
+    ('Advantix', null, '2022-08-04 00:00:00', 1, 1),
+    ('Vectra 3D', null, '2023-07-09 09:30:00', 1, 1);
+insert into public.anti_echinococcus_treatments (
+        name,
+        manufacturer,
+        administered_on,
+        administered_by,
+        pet_id
+    )
+values ('Fenpradin', null, '2022-05-16 00:00:00', 1, 1),
+    ('Fenpradin', null, '2022-06-15 00:00:00', 1, 1),
+    ('Cestal Plus', null, '2022-09-15 00:00:00', 1, 1),
+    ('Milpro', null, '2024-06-28 00:00:00', 1, 1),
+    ('Milprazon', null, '2025-03-21 00:00:00', 1, 1);
+insert into public.vaccinations (
+        name,
+        manufacturer,
+        lot_number,
+        expiry_date,
+        administered_on,
+        administered_by,
+        valid_from,
+        valid_until,
+        pet_id,
+        type
+    )
+values (
+        'DAPPi LR',
+        'Eurican',
+        'L492185',
+        '2023-02-24',
+        '2022-06-22',
+        1,
+        '2022-07-13',
+        '2023-06-22',
+        1,
+        'rabies'
+    ),
+    (
+        'DHPPi L4R',
+        'Biocan',
+        '35530',
+        '2024-09-01',
+        '2023-06-10',
+        1,
+        '2023-07-01',
+        '2024-06-10',
+        1,
+        'rabies'
+    ),
+    (
+        'LR',
+        'Biocan',
+        '295130A',
+        '2024-12-01',
+        '2024-06-04',
+        1,
+        '2024-06-25',
+        '2025-06-04',
+        1,
+        'rabies'
+    ),
+    (
+        'DHP',
+        'Nobivac',
+        'A145E01',
+        '2023-06-01',
+        '2022-05-06',
+        1,
+        null,
+        '2023-06-05',
+        1,
+        'other'
+    ),
+    (
+        'DHPPi',
+        'Nobivac',
+        'A668C01',
+        '2023-11-01',
+        '2022-05-22',
+        1,
+        null,
+        '2023-05-22',
+        1,
+        'other'
+    ),
+    (
+        'DAPPi LR',
+        'Eurican',
+        'L492185',
+        '2023-02-24',
+        '2022-06-22',
+        1,
+        null,
+        '2023-06-22',
+        1,
+        'other'
+    ),
+    (
+        'DHPPi L4R',
+        'Biocan',
+        '35530',
+        '2024-09-01',
+        '2023-06-10',
+        1,
+        null,
+        '2024-06-10',
+        1,
+        'other'
+    ),
+    (
+        'DHPPi',
+        'Biocan',
+        '655430',
+        '2025-01-01',
+        '2024-06-04',
+        1,
+        null,
+        '2025-06-04',
+        1,
+        'other'
+    );
