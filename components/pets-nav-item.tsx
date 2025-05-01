17f88e39b8072603ba5ec431c680184191864328
Vladislav Hadzhyiski <vladislav.hadzhiyski@gmail.com>
2025-05-01 20:19:07 +0300

    Refactor PetNavItem to remove useHashState and simplify navigation links; update imports and clean up code

--- components/pets-nav-item.tsx
+++ components/pets-nav-item.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import {
   SidebarMenuButton,
   SidebarMenuItem,
@@ -8,8 +9,6 @@ import {
   SidebarMenuSubButton,
   SidebarMenuSubItem,
 } from './ui/sidebar';
-import { useHashState } from '@passport/hooks/use-hash';
-import { usePathname } from 'next/navigation';
 
 export interface PetNavItemProps {
   pet: {
@@ -20,7 +19,6 @@ export interface PetNavItemProps {
 
 export default function PetNavItem({ pet }: PetNavItemProps) {
   const path = usePathname();
-  const [hash] = useHashState();
   return (
     <SidebarMenuItem>
       <SidebarMenuButton
@@ -32,48 +30,31 @@ export default function PetNavItem({ pet }: PetNavItemProps) {
       </SidebarMenuButton>
       <SidebarMenuSub className='ml-0 border-l-0 px-1.5'>
         <SidebarMenuSubItem>
-          <SidebarMenuSubButton
-            asChild
-            isActive={path === `/pets/${pet.id}` && hash === 'passport'}
-          >
+          <SidebarMenuSubButton asChild>
             <Link href={`/pets/${pet.id}#passport`}>Passport</Link>
           </SidebarMenuSubButton>
         </SidebarMenuSubItem>
         <SidebarMenuSubItem>
-          <SidebarMenuSubButton
-            asChild
-            isActive={path === `/pets/${pet.id}` && hash === 'vaccinations'}
-          >
+          <SidebarMenuSubButton asChild>
             <Link href={`/pets/${pet.id}#vaccinations`}>Vaccinations</Link>
           </SidebarMenuSubButton>
         </SidebarMenuSubItem>
         <SidebarMenuSubItem>
-          <SidebarMenuSubButton
-            asChild
-            isActive={
-              path === `/pets/${pet.id}` && hash === 'anti-echinococcus'
-            }
-          >
+          <SidebarMenuSubButton asChild>
             <Link href={`/pets/${pet.id}#anti-echinococcus`}>
               Anti-Echinococcus Treatments
             </Link>
           </SidebarMenuSubButton>
         </SidebarMenuSubItem>
         <SidebarMenuSubItem>
-          <SidebarMenuSubButton
-            asChild
-            isActive={path === `/pets/${pet.id}` && hash === 'anti-parasites'}
-          >
+          <SidebarMenuSubButton asChild>
             <Link href={`/pets/${pet.id}#anti-parasites`}>
               Anti-Parasite Treatments
             </Link>
           </SidebarMenuSubButton>
         </SidebarMenuSubItem>
         <SidebarMenuSubItem>
-          <SidebarMenuSubButton
-            asChild
-            isActive={path === `/pets/${pet.id}` && hash === 'examinations'}
-          >
+          <SidebarMenuSubButton asChild>
             <Link href={`/pets/${pet.id}#examinations`}>
               Clinical Examinations
             </Link>
